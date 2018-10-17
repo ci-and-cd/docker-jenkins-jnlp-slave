48997a250bbb80bf1ab0853e8e3122d733897d33
haolun zhang <opensource@infra.top>
2018-10-17 16:20:44 +0800

    fix variable IMAGE_PREFIX

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ WORK_DIR=$(pwd)
 
 if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
 
-export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo};
+export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo/};
 export IMAGE_NAME=${IMAGE_NAME:-jenkins-jnlp-slave}
 export IMAGE_TAG=${IMAGE_ARG_VERSION:-3.27}-${IMAGE_ARG_LOCALE:-en_US}.${IMAGE_ARG_ENCODING:-UTF-8}_${IMAGE_ARG_TZ_AREA:-Etc}.${IMAGE_ARG_TZ_ZONE:-UTC}-bionic
 if [ "${TRAVIS_BRANCH}" != "master" ]; then export IMAGE_TAG=${IMAGE_TAG}-SNAPSHOT; fi
@@ -20,7 +20,7 @@ sed "s#%IMAGE_ARG_ENCODING%#${IMAGE_ARG_ENCODING:-UTF-8}#g" Dockerfile_template 
   sed "s#%IMAGE_ARG_TZ_ZONE%#${IMAGE_ARG_TZ_ZONE:-UTC}#g" > Dockerfile
 
 # Build image
-if [[ "$(docker images -q ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q ${IMAGE_PREFIX}${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
     docker-compose build
 fi
 
